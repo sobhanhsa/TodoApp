453ba46c6e060ae8ee33a061b79c427496154f63
sobhanhs <sobhhanhsa1@gmail.com>
2023-06-30 16:18:58 +0330

    email validation

--- controllers/usercontroller.js
+++ controllers/usercontroller.js
@@ -1,16 +1,26 @@
 const {pool} = require("../database/dbquery")
 
+const {isEmailValid} = require("../validators/emailValidator")
+
 function signupHandler(req, res) {
     
     const body = req.body;
 
+    //check for empty body
     if ((!body.username) || (!body.email) || (!body.name) || (!body.password) 
         || (body.username === "") || (body.email === "") || (body.name === "") || (body.password === "")) {
         res.json({"data":{"message":"please input required fields"}})
         return
     }
 
-    
+    //check email
+    if (!isEmailValid(body.email)) {
+        res.json({"data":{"message":"invalid email"}})
+        return
+    }
+
+
+
 }
 
 module.exports = {
---
+++ validators/emailValidator.js
@@ -0,0 +1,23 @@
+let emailRegex = /^[-!#$%&'*+\/0-9=?A-Z^_a-z{|}~](\.?[-!#$%&'*+\/0-9=?A-Z^_a-z`{|}~])*@[a-zA-Z0-9](-*\.?[a-zA-Z0-9])*\.[a-zA-Z](-?[a-zA-Z0-9])+$/;
+function isEmailValid (email) {
+
+    if (email.length > 254)
+        return false;
+
+    let valid = emailRegex.test(email);
+    if (!valid)
+        return false;
+
+
+    let parts = email.split("@");
+    if (parts[0].length > 64)
+        return false;
+
+    let domainParts = parts[1].split(".");
+    if (domainParts.some(function (part) { return part.length > 63; }))
+        return false;
+
+    return true;
+}
+
+module.exports = {isEmailValid}
